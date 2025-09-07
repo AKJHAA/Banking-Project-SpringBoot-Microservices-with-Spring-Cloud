6cc7dfca99872735492b96b1cd1b5f9a9db71c03
Abhijeet Kumar <akjhaa1508@gmail.com>
2025-09-07 12:34:54 +0530

    Initial commit

---
+++ V1-SpringBoot/loans/src/main/java/com/abhi/loans/Repository/LoansRepository.java
@@ -0,0 +1,14 @@
+package com.abhi.loans.Repository;
+
+import com.abhi.loans.Entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
+
+}
